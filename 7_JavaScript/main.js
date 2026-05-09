4dfb532168faacf7e9c2112e55ea2d6d38ee61f0
CSRIT007 <you@example.com>
2026-05-09 09:26:21 +0700

    first commit

---
+++ 7_JavaScript/main.js
@@ -0,0 +1,17 @@
+// let admin, name;
+// name = "John Key";
+// admin = name;
+// alert( admin ); // John
+// This code assigns the string "John" to the variable 'name', then assigns the value of 'name' to 'admin', and finally displays the value of 'admin' in an alert dialog.
+// let ourPanetName = "Earth";
+// let currentUserName = "John";
+// // The variable names have been changed to use camelCase for better readability.
+// console.log(ourPanetName); // Earth
+// console.log(currentUserName); // John
+
+let student = {
+    firstName:"Alice",
+    lastName: "Johnson",
+    age:20,
+}
+alert(student); // Alice`
